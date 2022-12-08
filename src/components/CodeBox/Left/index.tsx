871791653efbe6eca470af9477d45dbea20439f7
linyan <1439713475@qq.com>
2022-12-08 15:13:20 +0800

    让colorInput的颜色和全局背景颜色统一

--- src/components/CodeBox/Left/index.tsx
+++ src/components/CodeBox/Left/index.tsx
@@ -1,17 +1,28 @@
-import React, { useRef } from "react";
+import React, { MutableRefObject, useRef } from "react";
 import { setBgColor } from "@/store/Setting";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 import "./index.css";
 
 const Left = () => {
   const dispatch = useDispatch();
+  const setting = useSelector((state: RootState) => state.setting);
 
   const handleClickColorBox = (event: any) => {
     dispatch(setBgColor(event.target.style.backgroundColor));
+    colorInput.current.value = setRgbtoHex(event.target.style.backgroundColor);
   };
 
-  const set16ToRgb = (str: string) => {
+  const setRgbtoHex = (color: string) => {
+    const rgb = color.split(",");
+    const r = parseInt(rgb[0].split('(')[1]);
+    const g = parseInt(rgb[1]);
+    const b = parseInt(rgb[2]);
+    return "#" + ((r << 16) + (g << 8) + b).toString(16).padStart(6, '0');
+  }
+
+  const setHexToRgb = (str: string) => {
     var reg = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
     if (!reg.test(str)) {
       return;
@@ -33,6 +44,8 @@ const Left = () => {
     return "rgb(" + arr.join(",") + ")";
   };
 
+  const colorInput: MutableRefObject<any> = useRef(null);
+
   return (
     <div className="code_box_left">
       <div className="code_box_left_content">
@@ -40,8 +53,9 @@ const Left = () => {
           type="color"
           className="color_input"
           onChange={(e) => {
-            dispatch(setBgColor(set16ToRgb(e.target.value)));
+            dispatch(setBgColor(setHexToRgb(e.target.value)));
           }}
+          ref={colorInput}
         />
         <div className="flex_between code_box_left_color_boxes">
           <div
