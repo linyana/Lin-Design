d1ba003d4a8890998023db95970b84a32eb97278
linyan <1439713475@qq.com>
2022-12-08 15:22:49 +0800

    封装了颜色列表

--- src/components/CodeBox/Left/index.tsx
+++ src/components/CodeBox/Left/index.tsx
@@ -1,13 +1,27 @@
 import React, { MutableRefObject, useRef } from "react";
 import { setBgColor } from "@/store/Setting";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/store";
+import { useDispatch } from "react-redux";
 
 import "./index.css";
 
+const colorList = [
+  "rgb(47, 121, 206)",
+  "rgb(173,181,189)",
+  "rgb(74, 211, 85)",
+  "rgb(209, 211, 74)",
+  "rgb(230, 147, 53)",
+];
+
+const colorList2 = [
+  "rgb(255,107,107)",
+  "rgb(34,184,207)",
+  "rgb(132,94,247)",
+  "rgb(224, 53, 230)",
+  "rgb(252,196,25)",
+];
+
 const Left = () => {
   const dispatch = useDispatch();
-  const setting = useSelector((state: RootState) => state.setting);
 
   const handleClickColorBox = (event: any) => {
     dispatch(setBgColor(event.target.style.backgroundColor));
@@ -16,11 +30,11 @@ const Left = () => {
 
   const setRgbtoHex = (color: string) => {
     const rgb = color.split(",");
-    const r = parseInt(rgb[0].split('(')[1]);
+    const r = parseInt(rgb[0].split("(")[1]);
     const g = parseInt(rgb[1]);
     const b = parseInt(rgb[2]);
-    return "#" + ((r << 16) + (g << 8) + b).toString(16).padStart(6, '0');
-  }
+    return "#" + ((r << 16) + (g << 8) + b).toString(16).padStart(6, "0");
+  };
 
   const setHexToRgb = (str: string) => {
     var reg = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
@@ -36,7 +50,7 @@ const Left = () => {
       }
       newStr = t;
     }
-    let arr = []; //将字符串分隔，两个两个的分隔
+    let arr = [];
     for (var i = 0; i < 6; i = i + 2) {
       let s = newStr.slice(i, i + 2);
       arr.push(parseInt("0x" + s));
@@ -58,58 +72,26 @@ const Left = () => {
           ref={colorInput}
         />
         <div className="flex_between code_box_left_color_boxes">
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "rgb(47, 121, 206)" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "#adb5bd" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "rgb(74, 211, 85)" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "rgb(209, 211, 74)" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "rgb(230, 147, 53)" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
+          {colorList.map((item) => {
+            return (
+              <div
+                className="code_box_left_color_box"
+                style={{ background: item }}
+                onClick={(event) => handleClickColorBox(event)}
+              ></div>
+            );
+          })}
         </div>
         <div className="flex_between">
-          <div
-            className="code_box_left_color_box"
-            style={{ backgroundColor: "#ff6b6b" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "#22b8cf" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "#845ef7" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "rgb(224, 53, 230)" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
-          <div
-            className="code_box_left_color_box"
-            style={{ background: "#fcc419" }}
-            onClick={(event) => handleClickColorBox(event)}
-          ></div>
+          {colorList2.map((item) => {
+            return (
+              <div
+                className="code_box_left_color_box"
+                style={{ background: item }}
+                onClick={(event) => handleClickColorBox(event)}
+              ></div>
+            );
+          })}
         </div>
       </div>
     </div>
