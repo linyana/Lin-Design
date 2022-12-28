b4ba3f5d9c5db083ccdf3d2af0c8ebaeba589cbc
LinYan <1439713475@qq.com>
2022-12-28 15:37:07 +0800

    feat: add card and card2

--- src/components/Components/module/Card/Component1/index.tsx
+++ src/components/Components/module/Card/Component1/index.tsx
@@ -5,62 +5,105 @@ import { codeState } from "@/store/Code";
 import "./index.css";
 
 const Component1 = () => {
-  const code: codeState = {
-    element: () => <div className="layout_component1_content1">content</div>,
-    HTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+	const code: codeState = {
+		element: () => (
+			<div className="card_component1_content">
+				<div className="card_title">标题</div>
+				<div className="card_text">
+					我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容
+				</div>
+			</div>
+		),
+		HTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  <div class="card_title">标题</div>
+  <div class="card_text">
+    我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容
+  </div>
 </div>`,
-    CSSCode: (
-      className: string,
-      bgColor: string,
-      color: string,
-      hoverColor: string
-    ) => {
-      return `div${className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+		CSSCode: (
+			className: string,
+			bgColor: string,
+			color: string,
+			hoverColor: string
+		) => {
+			return `div${className} {
+  cursor: pointer;
+  width: 260px;
+  padding-bottom: 30px;
+  min-height: 300px;
+  border-radius: 15px;
+  background-color: ${bgColor}
+  border: 1px solid rgba(148, 146, 146, 0.3);
+  transition: all 0.1s;
 }
-        `;
-    },
-    JSCode: () => `暂无`,
-    VueHTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  基本按钮
-</div>`,
-    VueJSCode: () => `暂无`,
-    ReactHTMLCode: (className: string) => `<div${
-      className.trim() ? ` className="${className.replace(".", "")}"` : ""
-    }>
-  基本按钮
-</div>`,
-    ReactJSCode: () => `暂无`,
-    TSCode: () => `暂无`,
-    VueTSCode: () => `暂无`,
-    ReactTSCode: () => `暂无`,
-  };
 
-  return (
-    <>
-      <div className="compontent_title" id="basic_button">
-        卡片
-      </div>
-      <p className="compontent_p">用于设置标题和文字</p>
-      <div className="component_box">
-        <div className="compontent_box_content">
-          <div className="card_component1_content">
-            <div className="card_top">标题</div>
-            <div className="card_bottom"></div>
-          </div>
-        </div>
-        <ShowCode code={code} />
-      </div>
-    </>
-  );
+div${className}:hover {
+  box-shadow: 0 0 8px rgba(0,0,0,0.1);
+}
+
+div${className} .card_title {
+  padding: 15px 30px;
+  font-size: 28px;
+  font-weight: 500;
+  border-bottom: 1px solid rgba(148, 146, 146, 0.3);
+}
+
+div${className} .card_text {
+  padding: 30px 30px 0;
+  font-size: 18px;
+  line-height: 30px;
+}`;
+		},
+		JSCode: () => `暂无`,
+		VueHTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  <div class="card_title">标题</div>
+  <div class="card_text">
+    我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容
+  </div>
+</div>`,
+		VueJSCode: () => `暂无`,
+		ReactHTMLCode: (className: string) => `<div${
+			className.trim() ? ` className="${className.replace(".", "")}"` : ""
+		}>
+  <div className="card_title">标题</div>
+  <div className="card_text">
+    我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容
+  </div>
+</div>`,
+		ReactJSCode: () => `暂无`,
+		TSCode: () => `暂无`,
+		VueTSCode: () => `暂无`,
+		ReactTSCode: () => `暂无`,
+	};
+
+	return (
+		<>
+			<div className="compontent_title" id="basic_button">
+				文字卡片
+			</div>
+			<p className="compontent_p">用于设置标题和文字</p>
+			<div className="component_box">
+				<div className="compontent_box_content">
+					<div
+						className="card_component1_content"
+						style={{ backgroundColor: "white" }}
+					>
+						<div className="card_title" style={{ color: "#495057" }}>
+							标题
+						</div>
+						<div className="card_text" style={{ color: "#495057" }}>
+							我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容
+						</div>
+					</div>
+				</div>
+				<ShowCode code={code} />
+			</div>
+		</>
+	);
 };
 
 export default Component1;
--- src/components/Components/module/Card/Component2/index.tsx
+++ src/components/Components/module/Card/Component2/index.tsx
@@ -4,63 +4,111 @@ import { codeState } from "@/store/Code";
 
 import "./index.css";
 
+import img from "@/assets/images/normal/img.jpg";
+
 const Component1 = () => {
-  const code: codeState = {
-    element: () => <div className="layout_component1_content1">content</div>,
-    HTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+	const code: codeState = {
+		element: () => (
+			<div className="card_component2_content">
+				<div className="card_title">标题</div>
+				<div className="card_img">
+					<img src={img} alt="请添加图片" />
+				</div>
+			</div>
+		),
+		HTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  <div class="card_title">标题</div>
+  <div class="card_img">
+    <img src="" alt="请添加图片" />
+  </div>
 </div>`,
-    CSSCode: (
-      className: string,
-      bgColor: string,
-      color: string,
-      hoverColor: string
-    ) => {
-      return `div${className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+		CSSCode: (
+			className: string,
+			bgColor: string,
+			color: string,
+			hoverColor: string
+		) => {
+			return `div${className} {
+  cursor: pointer;
+  width: 260px;
+  background-color: ${bgColor};
+  transition: all 0.1s;
+  border-radius: 15px;
+}
+
+div${className}:hover {
+  box-shadow: 0 0 8px rgba(0,0,0,0.1);
+}
+
+div${className} .card_title {
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border: 1px solid rgba(148, 146, 146, 0.3);
+  padding: 15px 30px;
+  font-size: 28px;
+  font-weight: 500;
+  border-bottom: 2px solid rgba(148, 146, 146, 0.3);
+  color: ${color}
+}
+
+div${className} .card_img img {
+  width: 100%;
+  vertical-align: bottom;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
 }
         `;
-    },
-    JSCode: () => `暂无`,
-    VueHTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  基本按钮
+		},
+		JSCode: () => `暂无`,
+		VueHTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  <div class="card_title">标题</div>
+  <div class="card_img">
+    <img src="" alt="请添加图片" />
+  </div>
 </div>`,
-    VueJSCode: () => `暂无`,
-    ReactHTMLCode: (className: string) => `<div${
-      className.trim() ? ` className="${className.replace(".", "")}"` : ""
-    }>
-  基本按钮
+		VueJSCode: () => `暂无`,
+		ReactHTMLCode: (className: string) => `<div${
+			className.trim() ? ` className="${className.replace(".", "")}"` : ""
+		}>
+  <div className="card_title">标题</div>
+  <div className="card_img">
+    <img src="" alt="请添加图片" />
+  </div>
 </div>`,
-    ReactJSCode: () => `暂无`,
-    TSCode: () => `暂无`,
-    VueTSCode: () => `暂无`,
-    ReactTSCode: () => `暂无`,
-  };
+		ReactJSCode: () => `暂无`,
+		TSCode: () => `暂无`,
+		VueTSCode: () => `暂无`,
+		ReactTSCode: () => `暂无`,
+	};
 
-  return (
-    <>
-      <div className="compontent_title" id="basic_button">
-        基本布局
-      </div>
-      <p className="compontent_p">利用margin来进行居中,建议自行设置宽度。</p>
-      <div className="component_box">
-        <div className="compontent_box_content">
-          <div className="card_component2_content">
-            <div className="card_top"></div>
-            <div className="card_bottom"></div>
-          </div>
-        </div>
-        <ShowCode code={code} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="compontent_title" id="basic_button">
+				带图片的卡片
+			</div>
+			<p className="compontent_p">用于有图片的情况。</p>
+			<div className="component_box">
+				<div className="compontent_box_content">
+					<div
+						className="card_component2_content"
+						style={{ backgroundColor: "white" }}
+					>
+						<div className="card_title" style={{ color: "#495057" }}>
+							标题
+						</div>
+						<div className="card_img">
+							<img src={img} alt="请添加图片" />
+						</div>
+					</div>
+				</div>
+				<ShowCode code={code} />
+			</div>
+		</>
+	);
 };
 
 export default Component1;
