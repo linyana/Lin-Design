7d3bdfec4dc56a8e703a9f88bd48d45867b7daca
LinYan <1439713475@qq.com>
2023-01-03 10:43:48 +0800

    feat: add title

--- src/components/Components/module/Title/Component1/index.tsx
+++ src/components/Components/module/Title/Component1/index.tsx
@@ -5,63 +5,68 @@ import { codeState } from "@/store/Code";
 import "./index.css";
 
 const Component1 = () => {
-  const code: codeState = {
-    element: () => <div className="layout_component1_content1">content</div>,
-    HTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+	const code: codeState = {
+		element: () => <div className="title_component1_content">标题</div>,
+		HTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  标题
 </div>`,
-    CSSCode: (
-      className: string,
-      bgColor: string,
-      color: string,
-      hoverColor: string
-    ) => {
-      return `${className === ''?'div':className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+		CSSCode: (
+			className: string,
+			bgColor: string,
+			color: string,
+			hoverColor: string
+		) => {
+			return `${className === "" ? "div" : className} {
+  font-size: 26px;
+  line-height: 60px;
+  color: ${color};
+  border-bottom: 1px solid ${bgColor};
+  font-weight: 700;
 }
         `;
-    },
-    JSCode: () => `暂无`,
-    VueHTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+		},
+		JSCode: () => `暂无`,
+		VueHTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  标题
 </div>`,
-    VueJSCode: () => `暂无`,
-    ReactHTMLCode: (className: string) => `<div${
-      className.trim() ? ` className="${className.replace(".", "")}"` : ""
-    }>
-  content
+		VueJSCode: () => `暂无`,
+		ReactHTMLCode: (className: string) => `<div${
+			className.trim() ? ` className="${className.replace(".", "")}"` : ""
+		}>
+  标题
 </div>`,
-    ReactJSCode: () => `暂无`,
-    TSCode: () => `暂无`,
-    VueTSCode: () => `暂无`,
-    ReactTSCode: () => `暂无`,
-  };
+		ReactJSCode: () => `暂无`,
+		TSCode: () => `暂无`,
+		VueTSCode: () => `暂无`,
+		ReactTSCode: () => `暂无`,
+	};
 
-  return (
-    <>
-      <div className="compontent_title" id="basic_button">
-        基本布局
-      </div>
-      <p className="compontent_p">利用margin来进行居中,建议自行设置宽度。</p>
-      <div className="component_box">
-        <div className="compontent_box_content">
-          <div className="title_component1_contents">
-            <div className="title_component1_content">
-
-            </div>
-          </div>
-        </div>
-        <ShowCode code={code} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="compontent_title" id="basic_button">
+				长下划线
+			</div>
+			<p className="compontent_p">在标题下方增加一条下划线增强视觉效果</p>
+			<div className="component_box">
+				<div className="compontent_box_content">
+					<div
+						className="title_component1_content"
+						style={{
+							borderBottom: "1px solid rgb(61, 60, 60)",
+							color: "#495057",
+						}}
+					>
+						标题
+					</div>
+				</div>
+				<ShowCode code={code} />
+			</div>
+		</>
+	);
 };
 
 export default Component1;
--- src/components/Components/module/Title/Component2/index.tsx
+++ src/components/Components/module/Title/Component2/index.tsx
@@ -5,59 +5,93 @@ import { codeState } from "@/store/Code";
 import "./index.css";
 
 const Component1 = () => {
-  const code: codeState = {
-    element: () => <div className="layout_component1_content1">content</div>,
-    HTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+	const code: codeState = {
+		element: () => (
+			<div className="title_component2_content">
+				标题
+				<div className="title_line"></div>
+			</div>
+		),
+		HTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  	标题
+	<div class="title_line"></div>
 </div>`,
-    CSSCode: (
-      className: string,
-      bgColor: string,
-      color: string,
-      hoverColor: string
-    ) => {
-      return `${className === ''?'div':className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+		CSSCode: (
+			className: string,
+			bgColor: string,
+			color: string,
+			hoverColor: string
+		) => {
+			return `${className === "" ? "div" : className} {
+  text-align: center;
+  font-size: 26px;
+  font-weight: 700;
+  line-height: 60px;
+  color: ${color};
+}
+
+${className === "" ? "div" : className}:hover {
+  color: ${hoverColor};
+}
+
+${className === "" ? "div" : className} .title_line {
+  height: 2px;
+  width: 100px;
+  margin: 10px auto;
+  background-color: ${bgColor};
+  border-radius: 50%;
 }
         `;
-    },
-    JSCode: () => `暂无`,
-    VueHTMLCode: (className: string) => `<div${
-      className.trim() ? ` class="${className.replace(".", "")}"` : ""
-    }>
-  content
+		},
+		JSCode: () => `暂无`,
+		VueHTMLCode: (className: string) => `<div${
+			className.trim() ? ` class="${className.replace(".", "")}"` : ""
+		}>
+  标题
+  <div class="title_line"></div>
 </div>`,
-    VueJSCode: () => `暂无`,
-    ReactHTMLCode: (className: string) => `<div${
-      className.trim() ? ` className="${className.replace(".", "")}"` : ""
-    }>
-  content
+		VueJSCode: () => `暂无`,
+		ReactHTMLCode: (className: string) => `<div${
+			className.trim() ? ` className="${className.replace(".", "")}"` : ""
+		}>
+  标题
+	<div className="title_line"></div>
 </div>`,
-    ReactJSCode: () => `暂无`,
-    TSCode: () => `暂无`,
-    VueTSCode: () => `暂无`,
-    ReactTSCode: () => `暂无`,
-  };
+		ReactJSCode: () => `暂无`,
+		TSCode: () => `暂无`,
+		VueTSCode: () => `暂无`,
+		ReactTSCode: () => `暂无`,
+	};
 
-  return (
-    <>
-      <div className="compontent_title" id="basic_button">
-        基本布局
-      </div>
-      <p className="compontent_p">利用margin来进行居中,建议自行设置宽度。</p>
-      <div className="component_box">
-        <div className="compontent_box_content">
-          <div className="layout_component1_content1">content</div>
-        </div>
-        <ShowCode code={code} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="compontent_title" id="basic_button">
+				居中下划线
+			</div>
+			<p className="compontent_p">利用margin来进行居中,建议自行设置宽度。</p>
+			<div className="component_box">
+				<div className="compontent_box_content">
+					<div
+						className="title_component2_content"
+						style={{
+							color: "#495057",
+						}}
+					>
+						标题
+						<div
+							className="title_line"
+							style={{
+								backgroundColor: "#495057",
+							}}
+						></div>
+					</div>
+				</div>
+				<ShowCode code={code} />
+			</div>
+		</>
+	);
 };
 
 export default Component1;
