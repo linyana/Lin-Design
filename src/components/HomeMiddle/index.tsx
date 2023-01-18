32c4995f3a1e39d9b1cdd7588f959b328f25980c
LinYan <1439713475@qq.com>
2023-01-18 14:58:05 +0800

    增加响应式

--- src/components/HomeMiddle/index.tsx
+++ src/components/HomeMiddle/index.tsx
@@ -1,26 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 import "./index.css";
 
 import banner from "@/assets/svg/normal/banner.svg";
 
 const Middle = () => {
-  return (
-    <div className="middle_padding box">
-      <div className="home_banner">
-        <div className="home_banner_img">
-          <img src={banner} alt="banner" />
-        </div>
-        <div className="home_banner_text">
-          <div className="home_banner_title">零封装组件库</div>
-          <div className="home_banner_title2">
-            无需阅读文档也能快速上手的组件库
-          </div>
-          <button>快速上手</button>
-        </div>
-      </div>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div className="middle_padding box">
+			<div className="home_banner">
+				<div className="home_banner_img">
+					<img src={banner} alt="banner" />
+				</div>
+				<div className="home_banner_text">
+					<div className="home_banner_title">零封装组件库</div>
+					<div className="home_banner_title2">
+						无需阅读文档也能快速上手的组件库
+					</div>
+					<button
+						onClick={() => {
+							navigate("/components/overview", {
+								replace: false,
+							});
+						}}
+					>
+						快速上手
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Middle;
