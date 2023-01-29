8255b008f153241727f2bc1eb6e7774930d59f0d
LinYan <1439713475@qq.com>
2023-01-29 17:20:52 +0800

    feat: add message2 and message3

--- src/components/Components/module/Message/Component2/index.tsx
+++ src/components/Components/module/Message/Component2/index.tsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ShowCode from "@/components/Components/ShowCode";
 import { codeState } from "@/store/Code";
 
 import wrongSrc from "@/assets/svg/normal/wrong.svg";
-import rightSrc from "@/assets/svg/normal/right.svg";
+import tipNoticeSrc from "@/assets/images/tip/tip-notice.png";
 
 import "./index.css";
 
-const Component2 = () => {
+const Component1 = () => {
 	const code: codeState = {
-		element: () => <div className="layout_component1_content1">content</div>,
+		element: () => (
+			<div className="message_component1_content">
+				<div className={noticeMessageClass}>
+					<img src={tipNoticeSrc} alt="" />
+					密码安全性较低
+				</div>
+				<button
+					onClick={() => {
+						showNoticeMessage();
+					}}
+				>
+					警告
+				</button>
+			</div>
+		),
 		HTMLCode: (className: string) => `<div${
 			className.trim() ? ` class="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div class="popup_message notice_message">
+    <img src="" alt="" />
+    密码安全性较低
+  </div>
+  <button onclick="showNoticeMessage()">警告</button>
 </div>`,
 		CSSCode: (
 			className: string,
@@ -21,36 +39,228 @@ const Component2 = () => {
 			color: string,
 			hoverColor: string
 		) => {
-			return `${className === "" ? "div" : className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+			return `
+${className === "" ? "div" : className} .popup_message {
+	padding: 0 48px;
+	height: 40px;
+	position: fixed;
+	top: 30px;
+	left: 50%;
+	transform: translateX(-50%);
+	border-radius: 6px;
+	z-index: 999999999;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	line-height: 40px;
+	backdrop-filter: blur(20px);
+	font-size: 14px;
+  display: none;
 }
-        `;
+
+${className === "" ? "div" : className} .popup_message img {
+	width: 16px;
+	height: 16px;
+	margin-right: 10px;
+}
+
+${className === "" ? "div" : className} .notice_message {
+	background-color: rgba(186, 233, 180, 0.6);
+	box-shadow: 0 0 10px rgba(132, 207, 122, 0.3);
+	color: rgb(113, 182, 127);
+}
+
+${className === "" ? "div" : className} .show_message {
+  display: block;
+	animation: showMessage 0.3s;
+}
+
+${className === "" ? "div" : className} .hidden_message {
+  display: block;
+	animation: hiddenMessage 0.3s;
+}
+
+@keyframes showMessage {
+	from {
+		transform: translateY(-50px) translateX(-50%);
+		opacity: 0;
+	}
+	to {
+		transform: translateY(0) translateX(-50%);
+		opacity: 1;
+	}
+}
+
+@keyframes hiddenMessage {
+	from {
+		transform: translateY(0) translateX(-50%);
+		opacity: 1;
+	}
+	to {
+		transform: translateY(-50px) translateX(-50%);
+		opacity: 0;
+	}
+}
+`;
 		},
-		JSCode: () => `暂无`,
+		JSCode:
+			() => `const noticeMessage = document.querySelector(".notice_message");
+
+const showNoticeMessage = () => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    noticeMessage.className = "popup_message notice_message show_message";
+    setTimeout(() => {
+      noticeMessage.className =
+        "popup_message notice_message hidden_message";
+      setTimeout(() => {
+        noticeMessage.className = "popup_message notice_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
 		VueHTMLCode: (className: string) => `<div${
 			className.trim() ? ` class="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div :class="noticeMessageClass">
+    <img src="" alt="" />
+    密码安全性较低
+  </div>
+  <button @click="showNoticeMessage">警告</button>
 </div>`,
-		VueJSCode: () => `暂无`,
+		VueJSCode: () => `import { ref } from "vue";
+const noticeMessageClass = ref("popup_message notice_message");
+
+const showNoticeMessage = () => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    noticeMessageClass.value = "popup_message notice_message show_message";
+    setTimeout(() => {
+      noticeMessageClass.value = "popup_message notice_message hidden_message";
+      setTimeout(() => {
+        noticeMessageClass.value = "popup_message notice_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
 		ReactHTMLCode: (className: string) => `<div${
 			className.trim() ? ` className="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div className={noticeMessageClass}>
+    <img src={tipRightSrc} alt="" />
+    密码安全性较低
+  </div>
+  <button
+    onClick={() => {
+      showNoticeMessage();
+    }}
+  >
+    警告
+  </button>
 </div>`,
-		ReactJSCode: () => `暂无`,
-		TSCode: () => `暂无`,
-		VueTSCode: () => `暂无`,
-		ReactTSCode: () => `暂无`,
+		ReactJSCode:
+			() => `	const [noticeMessageClass, setNoticeMessageClass] = useState<string>(
+  "popup_message notice_message"
+);
+
+const showNoticeMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    setNoticeMessageClass("popup_message notice_message show_message");
+    setTimeout(() => {
+      setNoticeMessageClass("popup_message notice_message hidden_message");
+      setTimeout(() => {
+        setNoticeMessageClass("popup_message notice_message");
+      }, 280);
+    }, 1200);
+  }
+};
+    `,
+		TSCode: () => `const noticeMessage = document.querySelector(".notice_message");
+
+const showNoticeMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    noticeMessage.className = "popup_message notice_message show_message";
+    setTimeout(() => {
+      noticeMessage.className =
+        "popup_message notice_message hidden_message";
+      setTimeout(() => {
+        noticeMessage.className = "popup_message notice_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
+		VueTSCode: () => `import { ref } from "vue";
+const noticeMessageClass = ref<string>("popup_message notice_message");
+
+const showNoticeMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    noticeMessageClass.value = "popup_message notice_message show_message";
+    setTimeout(() => {
+      noticeMessageClass.value = "popup_message notice_message hidden_message";
+      setTimeout(() => {
+        noticeMessageClass.value = "popup_message notice_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
+		ReactTSCode:
+			() => `	const [noticeMessageClass, setNoticeMessageClass] = useState<string>(
+  "popup_message notice_message"
+);
+
+const showNoticeMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    setNoticeMessageClass("popup_message notice_message show_message");
+    setTimeout(() => {
+      setNoticeMessageClass("popup_message notice_message hidden_message");
+      setTimeout(() => {
+        setNoticeMessageClass("popup_message notice_message");
+      }, 280);
+    }, 1200);
+  }
+};
+    `,
+	};
+
+	const [noticeMessageClass, setNoticeMessageClass] = useState<string>(
+		"popup_message notice_message"
+	);
+
+	const showNoticeMessage = (): void => {
+		if (
+			document.querySelector(".show_message") === null &&
+			document.querySelector(".hidden_message") === null
+		) {
+			setNoticeMessageClass("popup_message notice_message show_message");
+			setTimeout(() => {
+				setNoticeMessageClass("popup_message notice_message hidden_message");
+				setTimeout(() => {
+					setNoticeMessageClass("popup_message notice_message");
+				}, 280);
+			}, 1200);
+		}
 	};
 
 	return (
 		<>
 			<div className="compontent_title" id="message-anchor2">
-				警告提示
+				成功提示
 			</div>
 			<p className="function_p">
 				背景颜色：
@@ -66,11 +276,19 @@ const Component2 = () => {
 			</p>
 			<div className="component_box">
 				<div className="compontent_box_content">
-					<div className="layout_component1_content1">
-            <button>
-              警告
-            </button>
-          </div>
+					<div className="message_component2_content">
+						<div className={noticeMessageClass}>
+							<img src={tipNoticeSrc} alt="" />
+							密码安全性较低
+						</div>
+						<button
+							onClick={() => {
+								showNoticeMessage();
+							}}
+						>
+							警告
+						</button>
+					</div>
 				</div>
 				<ShowCode code={code} />
 			</div>
@@ -78,4 +296,4 @@ const Component2 = () => {
 	);
 };
 
-export default Component2;
+export default Component1;
--- src/components/Components/module/Message/Component3/index.tsx
+++ src/components/Components/module/Message/Component3/index.tsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ShowCode from "@/components/Components/ShowCode";
 import { codeState } from "@/store/Code";
 
 import wrongSrc from "@/assets/svg/normal/wrong.svg";
-import rightSrc from "@/assets/svg/normal/right.svg";
+import tipErrorSrc from "@/assets/images/tip/tip-error.png";
 
 import "./index.css";
 
-const Component3 = () => {
+const Component1 = () => {
 	const code: codeState = {
-		element: () => <div className="layout_component1_content1">content</div>,
+		element: () => (
+			<div className="message_component3_content">
+				<div className={errorMessageClass}>
+					<img src={tipErrorSrc} alt="" />
+					密码输入错误
+				</div>
+				<button
+					onClick={() => {
+						showErrorMessage();
+					}}
+				>
+					错误
+				</button>
+			</div>
+		),
 		HTMLCode: (className: string) => `<div${
 			className.trim() ? ` class="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div class="popup_message error_message">
+    <img src="" alt="" />
+    密码输入错误
+  </div>
+  <button onclick="showErrorMessage()">错误</button>
 </div>`,
 		CSSCode: (
 			className: string,
@@ -21,38 +39,229 @@ const Component3 = () => {
 			color: string,
 			hoverColor: string
 		) => {
-			return `${className === "" ? "div" : className} {
-  margin: auto;
-  min-height: 200px;
-  background-color: rgb(225, 227, 238);
-  padding: 10px;
+			return `
+${className === "" ? "div" : className} .popup_message {
+	padding: 0 48px;
+	height: 40px;
+	position: fixed;
+	top: 30px;
+	left: 50%;
+	transform: translateX(-50%);
+	border-radius: 6px;
+	z-index: 999999999;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	line-height: 40px;
+	backdrop-filter: blur(20px);
+	font-size: 14px;
+  display: none;
 }
-        `;
+
+${className === "" ? "div" : className} .popup_message img {
+	width: 16px;
+	height: 16px;
+	margin-right: 10px;
+}
+
+${className === "" ? "div" : className} .error_message {
+	background-color: rgba(186, 233, 180, 0.6);
+	box-shadow: 0 0 10px rgba(132, 207, 122, 0.3);
+	color: rgb(113, 182, 127);
+}
+
+${className === "" ? "div" : className} .show_message {
+  display: block;
+	animation: showMessage 0.3s;
+}
+
+${className === "" ? "div" : className} .hidden_message {
+  display: block;
+	animation: hiddenMessage 0.3s;
+}
+
+@keyframes showMessage {
+	from {
+		transform: translateY(-50px) translateX(-50%);
+		opacity: 0;
+	}
+	to {
+		transform: translateY(0) translateX(-50%);
+		opacity: 1;
+	}
+}
+
+@keyframes hiddenMessage {
+	from {
+		transform: translateY(0) translateX(-50%);
+		opacity: 1;
+	}
+	to {
+		transform: translateY(-50px) translateX(-50%);
+		opacity: 0;
+	}
+}
+`;
 		},
-		JSCode: () => `暂无`,
+		JSCode:
+			() => `const errorMessage = document.querySelector(".error_message");
+
+const showErrorMessage = () => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    errorMessage.className = "popup_message error_message show_message";
+    setTimeout(() => {
+      errorMessage.className =
+        "popup_message error_message hidden_message";
+      setTimeout(() => {
+        errorMessage.className = "popup_message error_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
 		VueHTMLCode: (className: string) => `<div${
 			className.trim() ? ` class="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div :class="errorMessageClass">
+    <img src="" alt="" />
+    密码输入错误
+  </div>
+  <button @click="showErrorMessage">错误</button>
 </div>`,
-		VueJSCode: () => `暂无`,
+		VueJSCode: () => `import { ref } from "vue";
+const errorMessageClass = ref("popup_message error_message");
+
+const showErrorMessage = () => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    errorMessageClass.value = "popup_message error_message show_message";
+    setTimeout(() => {
+      errorMessageClass.value = "popup_message error_message hidden_message";
+      setTimeout(() => {
+        errorMessageClass.value = "popup_message error_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
 		ReactHTMLCode: (className: string) => `<div${
 			className.trim() ? ` className="${className.replace(".", "")}"` : ""
 		}>
-  content
+  <div className={errorMessageClass}>
+    <img src={tipRightSrc} alt="" />
+    密码输入错误
+  </div>
+  <button
+    onClick={() => {
+      showErrorMessage();
+    }}
+  >
+    错误
+  </button>
 </div>`,
-		ReactJSCode: () => `暂无`,
-		TSCode: () => `暂无`,
-		VueTSCode: () => `暂无`,
-		ReactTSCode: () => `暂无`,
+		ReactJSCode:
+			() => `	const [errorMessageClass, setErrorMessageClass] = useState<string>(
+  "popup_message error_message"
+);
+
+const showErrorMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    setErrorMessageClass("popup_message error_message show_message");
+    setTimeout(() => {
+      setErrorMessageClass("popup_message error_message hidden_message");
+      setTimeout(() => {
+        setErrorMessageClass("popup_message error_message");
+      }, 280);
+    }, 1200);
+  }
+};
+    `,
+		TSCode: () => `const errorMessage = document.querySelector(".error_message");
+
+const showErrorMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    errorMessage.className = "popup_message error_message show_message";
+    setTimeout(() => {
+      errorMessage.className =
+        "popup_message error_message hidden_message";
+      setTimeout(() => {
+        errorMessage.className = "popup_message error_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
+		VueTSCode: () => `import { ref } from "vue";
+const errorMessageClass = ref<string>("popup_message error_message");
+
+const showErrorMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    errorMessageClass.value = "popup_message error_message show_message";
+    setTimeout(() => {
+      errorMessageClass.value = "popup_message error_message hidden_message";
+      setTimeout(() => {
+        errorMessageClass.value = "popup_message error_message";
+      }, 280);
+    }, 1200);
+  }
+};`,
+		ReactTSCode:
+			() => `	const [errorMessageClass, setErrorMessageClass] = useState<string>(
+  "popup_message error_message"
+);
+
+const showErrorMessage = (): void => {
+  if (
+    document.querySelector(".show_message") === null &&
+    document.querySelector(".hidden_message") === null
+  ) {
+    setErrorMessageClass("popup_message error_message show_message");
+    setTimeout(() => {
+      setErrorMessageClass("popup_message error_message hidden_message");
+      setTimeout(() => {
+        setErrorMessageClass("popup_message error_message");
+      }, 280);
+    }, 1200);
+  }
+};
+    `,
+	};
+
+	const [errorMessageClass, setErrorMessageClass] = useState<string>(
+		"popup_message error_message"
+	);
+
+	const showErrorMessage = (): void => {
+		if (
+			document.querySelector(".show_message") === null &&
+			document.querySelector(".hidden_message") === null
+		) {
+			setErrorMessageClass("popup_message error_message show_message");
+			setTimeout(() => {
+				setErrorMessageClass("popup_message error_message hidden_message");
+				setTimeout(() => {
+					setErrorMessageClass("popup_message error_message");
+				}, 280);
+			}, 1200);
+		}
 	};
 
 	return (
 		<>
 			<div className="compontent_title" id="message-anchor3">
-				失败提示
+				成功提示
 			</div>
-			<p className="compontent_p">利用margin来进行居中,建议自行设置宽度。</p>
 			<p className="function_p">
 				背景颜色：
 				<img src={wrongSrc} alt="" />
@@ -67,11 +276,19 @@ const Component3 = () => {
 			</p>
 			<div className="component_box">
 				<div className="compontent_box_content">
-					<div className="layout_component1_content1">
-            <button>
-              失败
-            </button>
-          </div>
+					<div className="message_component3_content">
+						<div className={errorMessageClass}>
+							<img src={tipErrorSrc} alt="" />
+							密码输入错误
+						</div>
+						<button
+							onClick={() => {
+								showErrorMessage();
+							}}
+						>
+							错误
+						</button>
+					</div>
 				</div>
 				<ShowCode code={code} />
 			</div>
@@ -79,4 +296,4 @@ const Component3 = () => {
 	);
 };
 
-export default Component3;
+export default Component1;
