9f21de586e02212704c81ac5457605552766dc13
LinYan <1439713475@qq.com>
2023-01-30 10:54:36 +0800

    feat: update overview

--- src/components/Components/module/Overview/index.tsx
+++ src/components/Components/module/Overview/index.tsx
@@ -8,21 +8,23 @@ import buttonSrc from "@/assets/images/overview/button.png";
 import layoutSrc from "@/assets/images/overview/layout.png";
 import inputSrc from "@/assets/images/overview/input.png";
 import titleSrc from "@/assets/images/overview/title.png";
-import slideSrc from "@/assets/images/overview/slide.png";
+import slideSrc from "@/assets/images/overview/slide.gif";
 import cardSrc from "@/assets/images/overview/card.png";
 import contentSrc from "@/assets/images/overview/content.png";
 import scrollBarSrc from "@/assets/images/overview/scrollBar.png";
 import switchSrc from "@/assets/images/overview/switch.png";
 import tableSrc from "@/assets/images/overview/table.png";
-
+import backTopSrc from "@/assets/images/overview/backTop.png";
+import navSrc from "@/assets/images/overview/nav.png";
+import messageSrc from "@/assets/images/overview/message.png";
 
 const Button = () => {
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const toTarget = (target: string): void => {
-		navigate(target,{
-			replace: false
-		})
-	}
+		navigate(target, {
+			replace: false,
+		});
+	};
 	return (
 		<div>
 			<div className="component_big_title">
@@ -32,7 +34,12 @@ const Button = () => {
 				通用
 			</div>
 			<div className="summary_cards">
-				<div className="summary_card" onClick={() => {toTarget('../layout')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../layout");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">布局</div>
 						<div className="summary_img_content">
@@ -43,7 +50,12 @@ const Button = () => {
 						<div className="summary_text">Layout</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../button')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../button");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">按钮</div>
 						<div className="summary_img_content">
@@ -54,7 +66,12 @@ const Button = () => {
 						<div className="summary_text">Button</div>
 					</div>
 				</div>
-        <div className="summary_card" onClick={() => {toTarget('../input')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../input");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">输入框</div>
 						<div className="summary_img_content">
@@ -65,7 +82,12 @@ const Button = () => {
 						<div className="summary_text">Input</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../title')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../title");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">标题</div>
 						<div className="summary_img_content">
@@ -76,7 +98,12 @@ const Button = () => {
 						<div className="summary_text">Title</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../scrollBar')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../scrollBar");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">滚动条</div>
 						<div className="summary_img_content">
@@ -87,7 +114,28 @@ const Button = () => {
 						<div className="summary_text">ScrollBar</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../switch')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../backTop");
+					}}
+				>
+					<div className="summary_content">
+						<div className="summary_title">返回顶部</div>
+						<div className="summary_img_content">
+							<div className="summary_img">
+								<img src={backTopSrc} alt="" />
+							</div>
+						</div>
+						<div className="summary_text">BackTop</div>
+					</div>
+				</div>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../switch");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">开关</div>
 						<div className="summary_img_content">
@@ -98,14 +146,35 @@ const Button = () => {
 						<div className="summary_text">Switch</div>
 					</div>
 				</div>
-        <span></span>
-        <span></span>
+				<span></span>
+				<span></span>
 			</div>
 			<div className="compontent_title" id="overview-anchor2">
 				内容展示
 			</div>
 			<div className="summary_cards">
-				<div className="summary_card" onClick={() => {toTarget('../slide')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../nav");
+					}}
+				>
+					<div className="summary_content">
+						<div className="summary_title">导航栏</div>
+						<div className="summary_img_content">
+							<div className="summary_img">
+								<img src={navSrc} alt="" />
+							</div>
+						</div>
+						<div className="summary_text">Nav</div>
+					</div>
+				</div>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../slide");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">轮播图</div>
 						<div className="summary_img_content">
@@ -116,7 +185,12 @@ const Button = () => {
 						<div className="summary_text">Slide</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../card')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../card");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">卡片</div>
 						<div className="summary_img_content">
@@ -127,7 +201,12 @@ const Button = () => {
 						<div className="summary_text">Card</div>
 					</div>
 				</div>
-        <div className="summary_card" onClick={() => {toTarget('../content')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../content");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">内容</div>
 						<div className="summary_img_content">
@@ -138,7 +217,12 @@ const Button = () => {
 						<div className="summary_text">Content</div>
 					</div>
 				</div>
-				<div className="summary_card" onClick={() => {toTarget('../table')}}>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../table");
+					}}
+				>
 					<div className="summary_content">
 						<div className="summary_title">表格</div>
 						<div className="summary_img_content">
@@ -149,8 +233,24 @@ const Button = () => {
 						<div className="summary_text">Table</div>
 					</div>
 				</div>
-        <span></span>
-        <span></span>
+				<div
+					className="summary_card"
+					onClick={() => {
+						toTarget("../message");
+					}}
+				>
+					<div className="summary_content">
+						<div className="summary_title">消息通知</div>
+						<div className="summary_img_content">
+							<div className="summary_img">
+								<img src={messageSrc} alt="" />
+							</div>
+						</div>
+						<div className="summary_text">Message</div>
+					</div>
+				</div>
+				<span></span>
+				<span></span>
 			</div>
 		</div>
 	);
