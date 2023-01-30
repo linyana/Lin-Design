1733a6a93dc682f4111e96ff90f1b032fbdc1351
LinYan <1439713475@qq.com>
2023-01-30 09:57:36 +0800

    统一风格

--- src/components/Main/Middle/index.tsx
+++ src/components/Main/Middle/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import topSrc from "@/assets/images/normal/top.png"
+
 import "./index.css";
 
 const App = () => {
-  return (
-    <div className="middle_content middle_padding">
-      <Outlet></Outlet>
-    </div>
-  );
+	return (
+		<>
+      <div className="to_top" onClick={() => {
+        window.scrollTo(0,0)
+      }}>
+        <img src={topSrc} alt="" />
+      </div>
+			<div className="middle_content middle_padding">
+				<Outlet></Outlet>
+			</div>
+		</>
+	);
 };
 
 export default App;
