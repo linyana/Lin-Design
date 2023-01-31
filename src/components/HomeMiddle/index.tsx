364369e72146b99cf6ff2f96237b77aed8f98957
LinYan <1439713475@qq.com>
2023-01-31 11:25:56 +0800

    feat: 优化index

--- src/components/HomeMiddle/index.tsx
+++ src/components/HomeMiddle/index.tsx
@@ -14,7 +14,9 @@ const Middle = () => {
 					<img src={banner} alt="banner" />
 				</div>
 				<div className="home_banner_text">
-					<div className="home_banner_title">零封装组件库</div>
+					<div className="home_banner_title_box">
+						<div className="home_banner_title">零封装组件库</div>
+					</div>
 					<div className="home_banner_title2">
 						无需阅读文档也能快速上手的组件库
 					</div>
